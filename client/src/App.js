6aa9f2b9189ef0e69433b7599564d31608bef08c
MohamadAlaskari <m_alskari96@hotmail.com>
2023-03-22 10:12:40 +0100

    install react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home, AboutMe } from './pages/indexpages'
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Aboutme" element={<AboutMe />} />
+      </Routes>
+    </Router>
+
   );
 }
 

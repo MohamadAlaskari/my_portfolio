109d675670e38dd5ae9592dbb320d86df41690a7
MohamadAlaskari <m_alskari96@hotmail.com>
2023-03-26 11:12:43 +0200

    Projects page

--- client/src/pages/Projects/Projects.jsx
+++ client/src/pages/Projects/Projects.jsx
@@ -4,12 +4,52 @@ const Projects = () => {
     return (
         <div className='container'>
             <div className="row">
-                <div  className="col-md-4">
-                    <h2>Projects</h2>
+
+
+
+
+                <h2>Selected Works</h2>
+                <div className="projects-items">
+                    <div className="project-item">
+                        <div className="projects-img-container">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp5-jcPdioheEwSWoVbHL0v0q4RQoDcnQJsg&usqp=CAU" alt="" />
+                        </div>
+                        <h5>Cyporg app - React app</h5>
+                        <p>2023</p>
                     </div>
-                <div  className="col-md-8"></div>
+                    <div className="project-item">
+                        <div className="projects-img-container">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp5-jcPdioheEwSWoVbHL0v0q4RQoDcnQJsg&usqp=CAU" alt="" />
+                        </div>
+                        <h5>Cyporg app - React app</h5>
+                        <p>2023</p>
+                    </div>
+                    <div className="project-item">
+                        <div className="projects-img-container">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp5-jcPdioheEwSWoVbHL0v0q4RQoDcnQJsg&usqp=CAU" alt="" />
+                        </div>
+                        <h5>Cyporg app - React app</h5>
+                        <p>2023</p>
+                    </div>
+                    <div className="project-item">
+                        <div className="projects-img-container">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp5-jcPdioheEwSWoVbHL0v0q4RQoDcnQJsg&usqp=CAU" alt="" />
+                        </div>
+                        <h5>Cyporg app - React app</h5>
+                        <p>2023</p>
+                    </div>
+                    <div className="project-item">
+                        <div className="projects-img-container">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp5-jcPdioheEwSWoVbHL0v0q4RQoDcnQJsg&usqp=CAU" alt="" />
+                        </div>
+                        <h5>Cyporg app - React app</h5>
+                        <p>2023</p>
+                    </div>
+
+                </div>
             </div>
         </div>
+
     )
 }
 
